41e7f35c53aa3350ac7426b192edc92f3818f85c
Erica Barnell <ebarnell@GSCs-MacBook-Pro-2.local>
2017-06-08 09:43:04 -0500

    changes

--- Probe_Selection.py
+++ Probe_Selection.py
@@ -4,26 +4,36 @@
 
 
 """Probe_Selection.py will pull existing variants from the CIViC Knowledgebase, iterate through all pulled variants, and divide variants into lists.  
+
+Usage: Probe_Selection.py <threshold>
+
+<threshold> = CIViC Score Cutoff
+
+
 The output will be four lists of variants:
 1) Not_Evaluated: Probes that will not be evaluated in the Biomarker Capture Panel
 2) NanoString_Probes_Needed: Probes that will need to be evaluated using NanoString Technology
 3) Capture_Sequence_Probes_Needed: Probes that will need to be evaluated using Capture-Sequencing Technology
 4) Biomarker_Probe_Already_Created: Probe that have already been designed and validated
 Each list will contain four columns.  These columns will be "name" "chromosome" "start" "stop". 
-Usage: Probe_Selection.py
+
+
+
 """
 
 ##Pull in Data from JSON
 #!/usr/bin/env python3
 import json, requests
+import sys
 
-variant_dict = {}
+variant_dict = []
 
-variant_list = requests.get('https://civic.genome.wustl.edu/api/panels/captureseq/qualifying_variants?minimum_score=30').json()['records']
+variant_list = requests.get('https://civic.genome.wustl.edu/api/panels/captureseq/qualifying_variants?minimum_score=' + sys.argv[1]).json()['records']
 
-print(variant_list)
-
-
+for k in range(0, len(variant_list)):
+    if variant_list[k]['entrez_name'] not in variant_dict:
+        variant_dict.append(variant_list[k]['entrez_name'])
+print(len(variant_dict))
 
 
 
