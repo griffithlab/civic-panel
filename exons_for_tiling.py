c459377db2d94adc6ae5abea6f9bbf32799567c6
Erica Barnell <ebarnell@GSCs-MacBook-Pro-2.local>
2017-08-01 13:34:16 -0500

    clean up the box and add al of the bed files for exon analysis

--- exons_for_tiling.py
+++ exons_for_tiling.py
@@ -7,13 +7,12 @@
 This python code takes in a list of genes and the coordinates for all transcripts and creates a text file with chr, start
 stop, gene_name for all exons.
 
-Usage: exons_for_tiling.py <genes_to_be_tiled> <bed file with transcripts>
+Usage: exons_for_tiling.py <genes_to_be_tiled>
 
-python3 exons_for_tiling.py genes.txt UCSC_exons_modif_canonical.bed 
+python3 exons_for_tiling.py genes_to_be_tiled.txt
 
 Input Files:
-<genes_to_be_tiled> = list of gene names (HUGO) that need to be tiled
-<bed file with transcripts> = UCSC_exons_modif_canonical.bed for canonical transcripts or other for full tile
+<genes_to_be_tiled> = list of gene names (ENST) that need to be tiled
 
 Output Files:
 <list of exons> = coordinates for all exons in your gene list called  exons_coordinates.txt
@@ -26,36 +25,19 @@ Output Files:
 import json, requests
 import sys
 
-#Create an empty list for all of the imported transcript coordinates
-coordinates = []
-
-#iterate through the coordinates and pull chr, start, stop, name
-transcripts = open(sys.argv[2], 'r')
-for line in transcripts:
-    line = line.strip('\n')
-    line = line.split('\t')
-    coordinates.append([line[0], line[1], line[2], line[3]])
-
 #create an empty list of all the genes that need to be tiled
 genes_list = []
 #iterate through gene list and add gene names to the list
 genes = open(sys.argv[1], 'r')
 for item in genes:
     item = item.strip('\n')
-    genes_list.append(item)
-
-#create an empty list for the final coordinates for the genes that need to be tiled
-exons = []
-#link both spreadsheets by name and append coordinates to the exons
-for item in genes_list:
-    for k in coordinates:
-        if item == k[3]:
-            exons.append(k)
+    item = item.split('.')
+    if item[0] not in genes_list:
+        genes_list.append(item[0])
 
 #create output file for the exon coordinates for all genes that need to be tiled
-final = open('exons_coordinates.txt', 'w')
-for item in exons: #iterate through nanostring list
-    for k in item:
-        final.write(str(k) + '\t')
+final = open('ENST_to_be_tiled', 'w')
+for item in genes_list: #iterate through nanostring list
+    final.write(str(item) + '\t')
     final.write('\n')
 final.close()
