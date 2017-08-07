8ee1f770fe99d4e97682e07084ef93f4e6a92e2f
Erica Barnell <ebarnell@gscs-mbp-2.attlocal.net>
2017-08-07 14:44:53 -0500

    updates

--- exon_tiling.R
+++ exon_tiling.R
@@ -2,7 +2,8 @@ setwd('/Users/ebarnell/civic-panel/')
 
 #Import Libraries
 library(biomaRt)
-library(bedr)
+library(data.table)
+library('bedr')
 
 
 #Read in list of ENSTs with version numbers of all genes that need to be tiled
@@ -43,7 +44,68 @@ ENST_protein_coding_fewer_UTRs <- ENST_protein_coding_UTRs[-which((ENST_protein_
                                                                   ), 
                                                            ]
 
-ENST_protein_coding_no_UTRs <- ENST_protein_coding_fewer_UTRs[-which()
-  
-]
+ENST_protein_coding_fewer_UTRs$exon_chrom_end <- apply(ENST_protein_coding_fewer_UTRs, 1, function(x){
+  if(!is.na(x['5_utr_end']) & 
+     !is.na(x['exon_chrom_end']) &
+     x['5_utr_end'] == x['exon_chrom_end']){
+       return(x['5_utr_start']-1)
+     } else {
+       return(x['exon_chrom_end'])
+     }
+})
 
+ENST_protein_coding_fewer_UTRs$exon_chrom_start <- apply(ENST_protein_coding_fewer_UTRs, 1, function(x){
+  if(!is.na(x['5_utr_start']) & 
+     !is.na(x['exon_chrom_start']) &
+     x['5_utr_start'] == x['exon_chrom_start']){
+    return(x['5_utr_end']+1)
+  } else {
+    return(x['exon_chrom_start'])
+  }
+})
+
+ENST_protein_coding_fewer_UTRs$exon_chrom_end <- apply(ENST_protein_coding_fewer_UTRs, 1, function(x){
+  if(!is.na(x['3_utr_end']) & 
+     !is.na(x['exon_chrom_end']) &
+     x['3_utr_end'] == x['exon_chrom_end']){
+    return(x['3_utr_start']-1)
+  } else {
+    return(x['exon_chrom_end'])
+  }
+})
+
+ENST_protein_coding_fewer_UTRs$exon_chrom_start <- apply(ENST_protein_coding_fewer_UTRs, 1, function(x){
+  if(!is.na(x['3_utr_start']) & 
+     !is.na(x['exon_chrom_start']) &
+     x['3_utr_start'] == x['exon_chrom_start']){
+    return(x['3_utr_end']+1)
+  } else {
+    return(x['exon_chrom_start'])
+  }
+})
+
+
+ENST_protein_coding_no_UTRs_bed <- ENST_protein_coding_fewer_UTRs[1:3]
+  
+ENST_protein_coding_no_UTRs_bed_ordered <- ENST_protein_coding_no_UTRs_bed[with(
+  ENST_protein_coding_no_UTRs_bed, order(chromosome_name, exon_chrom_start)), ]
+
+
+ENST_protein_coding_no_UTRs_bed_merged <- if (check.binary("bedtools")) {
+  index <- ENST_protein_coding_no_UTRs_bed_ordered;
+  a <- index[[1]];
+  a.sort <- bedr.sort.region(a);
+  a.merged <- bedr.merge.region(a.sort);
+}
+
+ENST_protein_coding_no_UTRs_bed_merged <- bedr.merge.region(ENST_protein_coding_no_UTRs_bed_ordered)
+
+
+
+
+if (check.binary("bedr")) {
+  index <- get.example.regions();
+  a <- index[[1]];
+  a.sort <- bedr.sort.region(a);
+  a.merged <- bedr.merge.region(a.sort);
+}
