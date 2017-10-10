cbcd7d06c344ebca9058ebeac2d81517caecfd21
Erica Barnell <ebarnell@vpn-10-1-24-164.gsc.wustl.edu>
2017-10-10 16:50:50 -0500

    update

---
+++ panel_samples/CIViC_overlap.py
@@ -0,0 +1,19 @@
+#Erica Barnell
+#6.7.2017
+#!/usr/bin/env python3
+
+
+"""
+Information: CIViC_overlap.py evaluates the clinically relevant variants missed in the error_data
+
+Usage: python3 CIViC_overlap.py <error_data>
+
+Example: python3 Probe_Selection.py 20 panel_genes.txt tile_classification.txt
+
+1) <error_data> = list of classification
+
+Output: 
+
+"""
+
+
--- panel_samples/overlap.py
+++ panel_samples/overlap.py
@@ -35,28 +35,28 @@ sample1 = append_file_to_list(sys.argv[2])
 
 mutation_overlap = {}
 count = 0
-header = sample1[0]
-sample = header.index('sample')
-gene = header.index('gene_name')
-VAF = header.index('VAF')
-variant = header.index('amino_acid')
+# header = sample1[0]
+# sample = header.index('sample')
+# gene = header.index('gene_name')
+# VAF = header.index('VAF')
+# variant = header.index('amino_acid')
 
 for item in sample1:
     for exon in civic_coordinates:
-        if str(item[0]) == str(exon[0]) and int(item[1]) >= int(exon[1]) and int(item[2]) <= int(exon[2]): #and item[40] == 'tumor':
+        if str(item[0]) == str(exon[0]) and int(item[1]) >= int(exon[1]) and int(item[2]) <= int(exon[2]):
             count += 1
-            if item[sample] not in mutation_overlap:
-                mutation_overlap[item[sample]] = [0]
-                mutation_overlap[item[sample]][0] += 1
-                mutation_overlap[item[sample]].append(item[gene])
-                mutation_overlap[item[sample]].append(item[variant])
-                mutation_overlap[item[sample]].append(item[VAF])
+            # if item[sample] not in mutation_overlap:
+            #     mutation_overlap[item[sample]] = [0]
+            #     mutation_overlap[item[sample]][0] += 1
+            #     mutation_overlap[item[sample]].append(item[gene])
+            #     mutation_overlap[item[sample]].append(item[variant])
+            #     mutation_overlap[item[sample]].append(item[VAF])
 
-            else:
-                mutation_overlap[item[sample]][0] += 1
-                mutation_overlap[item[sample]].append(item[gene])
-                mutation_overlap[item[sample]].append(item[variant])
-                mutation_overlap[item[sample]].append(item[VAF])
+            # else:
+            #     mutation_overlap[item[sample]][0] += 1
+                # mutation_overlap[item[sample]].append(item[gene])
+                # mutation_overlap[item[sample]].append(item[variant])
+                # mutation_overlap[item[sample]].append(item[VAF])
 
 for k,v in mutation_overlap.items():
     print(k, v)
@@ -64,3 +64,4 @@ for k,v in mutation_overlap.items():
 print('Total number of overlaps:', count)
 print('Total number of exons in CIViC', len(civic_coordinates))
 print('Total number of mutations in all samples', len(sample1))
+
