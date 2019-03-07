a91c44419cd8295c8d6243351d3263681bc1e319
ebarnell <erica.barnell@gmail.com>
2019-03-07 10:13:31 -0600

    add test file for jupyter integration

---
+++ docs/TEST.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+This is a test python file for the CIViC smMIPs paper
+
+Usage: python3 TEST.py <name>
+
+Arguments:
+    <name> = List your name
+"""
+
+
+import sys
+import numpy as np
+
+person = sys.argv[0]
+
+print('Your name is', + person)
+
+
