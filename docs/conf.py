95772b6a3e8c6146fba24510fe99e99968d6453f
ebarnell <erica.barnell@gmail.com>
2019-03-07 12:19:06 -0600

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -82,9 +82,13 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-#html_theme_options = {
-#'navigation_depth: 3' 
-#}
+
+html_theme_options = {
+   'header_inverse': False,
+   'relbar_inverse': False,
+   'inner_theme': True,
+   'inner_theme_name': 'bootswatch-cosmo',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
