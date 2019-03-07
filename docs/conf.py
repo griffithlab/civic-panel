9eb9b1b5db951421e58d2c54b43712ba26e07334
ebarnell <erica.barnell@gmail.com>
2019-03-07 12:05:58 -0600

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -78,13 +78,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'basicstrap'
-html_theme_options = {
-   'header_inverse': False,
-   'relbar_inverse': False,
-   'inner_theme': True,
-   'inner_theme_name': 'bootswatch-cosmo',
-}
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
