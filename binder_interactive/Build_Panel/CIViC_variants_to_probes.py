baca4ce093386e9be336f91c4148be89ec7c478c
ebarnell <erica.barnell@gmail.com>
2019-04-05 20:43:43 -0500

    update build

--- binder_interactive/Build_Panel/CIViC_variants_to_probes.py
+++ binder_interactive/Build_Panel/CIViC_variants_to_probes.py
@@ -45,12 +45,16 @@ def create_probe_list(CIViC_variants):
     civic_coordinate_variants = ['DNA BINDING DOMAIN MUTATION', 'PROMOTER DEMETHYLATION', 'CONSERVED DOMAIN MUT']
     probes_list = []
     for i,row in CIViC_variants.iterrows():
+        if row['Reference Build'] != "GRCh37":
+            print('Variant Reference Build not in GRCh37: SKIPPING VARIANT' + row['Gene'] + ' ' + row['Variant Name'])
+            break
         #Iterate through each variant an pull values
-        variant = row['variant']
-        gene = row['gene']
-        chrom = 'chr' + str(row['chromosome'])
-        start = int(row['start'])
-        stop = int(row['stop'])
+
+        variant = row['Variant Name']
+        gene = row['Gene']
+        chrom = 'chr' + str(row['Chromosome 1'])
+        start = int(row['Chromosome 1 Start'])
+        stop = int(row['Chromosome 1 Stop'])
         difference = int(stop) - int(start)
         
         #if the variant requires few probes for capture
@@ -114,7 +118,74 @@ def create_probe_list(CIViC_variants):
                     start = int(row[1])
                     stop = int(row[2])
                     probes_list.append([chromosome, start, stop, variant, gene, str(i + 1) + '-' + gene + '_' + variant, variant_type])
-                
+        if 'Chromosome 2':
+            chrom = 'chr' + str(row['Chromosome 2'])
+            start = int(row['Chromosome 2 Start'])
+            stop = int(row['Chromosome 2 Stop'])
+            difference = int(stop) - int(start)
+
+            #if the variant requires few probes for capture
+            if difference <= 250:
+                #create probe for hotspot variant
+                variant_type = 'hotspot coverage'
+                probes_list.append([chrom, start, stop + 1, variant, gene, str(1) + '-' + gene + '_' + variant, variant_type])        
+
+            # If variant type is in a regulatory region or UTR
+            elif 'INTRON' in variant or 'UTR' in variant:
+                variant_type = 'sparse tiling'
+                probes_list.append([chrom, start, stop, variant, gene, str(1) + '-' + gene + '_' + variant, variant_type])
+
+            # for variant specific variant types pull CIViC coordiantes
+            elif variant in civic_coordinate_variants:
+                variant_type = 'sparse tiling'
+                probes_list.append([chrom, start, stop, variant, gene, str(1) + '-' + gene + '_' + variant, variant_type])
+
+            #If the transcript requires full tiling
+            else:
+
+                #Pull ESNG ID using the HUGO gene name
+                ESNG_ID = list(matching_list[matching_list['Gene name'] == gene]['Gene stable ID'])[0]
+
+                #query biomart for all protein coding regions
+                all_exons = pd.DataFrame(dataset.query(attributes=['chromosome_name','genomic_coding_start','genomic_coding_end'], filters={'link_ensembl_gene_id': [ESNG_ID]})).dropna()
+
+                #Sort the values by start position for bedtools format
+                all_exons = all_exons.sort_values(by=['Genomic coding start'])
+
+                #Force start and stop coordinates to integer (not float)
+                all_exons['Genomic coding start'] = all_exons['Genomic coding start'].astype(int)
+                all_exons['Genomic coding end'] = all_exons['Genomic coding end'].astype(int)
+
+                #Merge all values for all exons in protein coding regions
+                final_region = pybedtools.BedTool.from_dataframe(all_exons).merge().to_dataframe()
+
+                #Parse down the required coverage for variants in sparse_tile_variants
+                if variant in sparse_tile_variant_types:
+                    #change varinat type to 'full tiling'
+                    variant_type = 'sparse tiling'
+                    #Create a probe for each consecutive genomic region across large variant
+                    for i,row in final_region.iterrows():
+                        chromosome = 'chr' + str(row[0])
+                        #find the middle of each exon
+                        middle = (row[1] + row[2]) / 2
+                        #add 60 bp above and below middle region
+                        start = int(middle - 60)
+                        stop = int(middle + 60)
+                        #append this genomic region to list
+                        probes_list.append([chromosome, start, stop, variant, gene, str(i + 1) + '-' + gene + '_' + variant, variant_type])
+
+
+                #Else complete full tiling
+                else:
+                    #change varinat type to 'full tiling'
+                    variant_type = 'full tiling'
+                    #Create a probe for each consecutive genomic region across large variant
+                    for i,row in final_region.iterrows():
+                        chromosome = 'chr' + str(row[0])
+                        start = int(row[1])
+                        stop = int(row[2])
+                        probes_list.append([chromosome, start, stop, variant, gene, str(i + 1) + '-' + gene + '_' + variant, variant_type]) 
+        
     #Create pandas dataframe of probe list
     probes = pd.DataFrame(probes_list)
     return probes
